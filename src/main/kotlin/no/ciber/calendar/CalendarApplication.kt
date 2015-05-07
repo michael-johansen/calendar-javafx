66f259ad7268980a88a0c7a6ddffa0b2b9976041
Michael Johansen <michaeljohansen83@gmail.com>
2015-05-07 19:44:33 +0200

    Converting to frame: WIP

--- src/main/kotlin/no/ciber/calendar/CalendarApplication.kt
+++ src/main/kotlin/no/ciber/calendar/CalendarApplication.kt
@@ -9,6 +9,7 @@ import javafx.scene.Group
 import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.scene.control.Label
+import javafx.scene.layout.BorderPane
 import javafx.stage.Stage
 import no.ciber.calendar.model.SearchMode
 import org.slf4j.LoggerFactory
@@ -19,6 +20,7 @@ import java.util.ResourceBundle
 class CalendarApplication : Application() {
     val logger = LoggerFactory.getLogger(javaClass)
     val scene = Scene(Group(Label("loading")), 600.0, 800.0)
+    var frame: BorderPane = BorderPane()
     var locale = Locale.ENGLISH;
 
     override fun start(primaryStage: Stage) {
@@ -27,16 +29,28 @@ class CalendarApplication : Application() {
         primaryStage.addEventHandler(Event.ANY, applicationEventHandler())
         primaryStage.show()
 
-        logger.info("Navigating to event list")
-        primaryStage.fireEvent(NavigateToLogin())
+        Platform.runLater({
+            val frame = loadParent("layouts/frame.fxml") as BorderPane
+            this.frame = frame
+            scene.setRoot(frame)
+
+            logger.info("Navigating to event list")
+            primaryStage.fireEvent(NavigateToLogin())
+        })
     }
 
     fun gotoView(fxml: String, vararg arguments: Any) {
+        Platform.runLater({
+            frame.setCenter(loadParent(fxml, *arguments))
+        })
+    }
+
+    private fun loadParent(fxml: String, vararg arguments: Any): Parent? {
         logger.info("Navigating to view $fxml, with arguments: ${arguments.toList()}")
         fun defaultControllerFactory(clazz: Class<*>): Any {
             logger.info("Initializing controller $clazz with ${arguments.toList()}")
-            val arrayOfClasss = arguments.map { it.javaClass }.copyToArray()
-            return clazz.getConstructor(* arrayOfClasss).newInstance(* arguments)!!
+            val argumentTypes = arguments.map { it.javaClass }.copyToArray()
+            return clazz.getConstructor(* argumentTypes).newInstance(* arguments)!!
         }
 
         val loader = FXMLLoader()
@@ -44,9 +58,10 @@ class CalendarApplication : Application() {
         loader.setLocation(getRequiredResource(fxml))
         loader.setResources(ResourceBundle.getBundle("messages", locale))
         loader.setControllerFactory (::defaultControllerFactory)
-
-        Platform.runLater({ scene.setRoot(loader.load<Parent>()) })
+        val parent = loader.load<Parent>()
+        return parent
     }
+
 
     private fun getRequiredResource(fxml: String): URL {
         return javaClass<CalendarApplication>().getResource(fxml)!!
---
+++ src/main/kotlin/no/ciber/calendar/controllers/FrameController.java
@@ -0,0 +1,8 @@
+package no.ciber.calendar.controllers;
+/**
+ * User: Michael Johansen
+ * Date: 07.05.2015
+ * Time: 15:32
+ */
+public class FrameController {
+}
